37a982ae2df0ff38bb9234d62526ab670b3e67f2
JorgenFreeman <oliverdolan@msn.com>
2017-04-19 01:24:16 +0100

    Touch up main menu

---
+++ Assets/Code/Scripts/lookAtTarget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAtTarget : MonoBehaviour 
+{
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	    if(target != null)
+            transform.LookAt(target);
+
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
